529c505900493f9a71a7cc8a87fe4c9fa46fcc3b
Abhinandan G <abhigouli032@gmail.com>
2023-12-02 16:30:18 +0530

    Added comments to the code

--- ytdownload.py
+++ ytdownload.py
@@ -2,11 +2,13 @@ from pytube import YouTube
 from sys import argv
 from plyer import notification
 
+#link passed as arguement in command line
 link = argv[1]
 
+#setting download path
 download_path = "C:/Users/HP/Downloads"
 
-
+#Function for progress bar in command prompt
 def progress(streams, chunk: bytes, bytes_remaining: int):
     contentsize = video.filesize
     size = contentsize - bytes_remaining
@@ -16,18 +18,26 @@ def progress(streams, chunk: bytes, bytes_remaining: int):
     
 try:
     
+    #creating object of YouTube class
     yt=YouTube(link,on_progress_callback=progress)
 
+    #Calculating file size
     File_size=yt.streams.get_highest_resolution().filesize_mb
+
+    #Notifing user
     notification.notify(
         title = f"Downloading {yt.title}",
         message = f"File size : {File_size}",
         app_icon = "C:/Users/HP/OneDrive/Desktop/yt-download/pythonlogoIco.ico"
     )
 
+    #Getting highest resolution of video
     video=yt.streams.get_highest_resolution();
+
+    #Downloading the video
     video.download(output_path=download_path)
 
+    #Notifing user
     notification.notify(
         title = yt.title,
         message = f"Download complete \n {download_path}",
--- ytplaylistdownload.py
+++ ytplaylistdownload.py
@@ -3,6 +3,7 @@ from sys import argv
 from plyer import notification
 import time
 
+#Function for progress bar in command prompt
 def progress(streams, chunk: bytes, bytes_remaining: int):
     contentsize = high_def_video.filesize
     size = contentsize - bytes_remaining
@@ -10,12 +11,16 @@ def progress(streams, chunk: bytes, bytes_remaining: int):
     print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
     '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
 
+#link passed as arguement in command line
 link = argv[1]
 
+#setting download path
 download_path = "C:/Users/HP/Downloads"
 
+#creating object of playlist class
 p = Playlist(link)
 
+#Notifing user
 print(f"Title : {p.title}")
 notification.notify(
         title = f"Downloading {(p.title)}",
@@ -26,18 +31,24 @@ notification.notify(
 
 time.sleep(3)
 
+#Iterating over each video in playlist
 for video in p.videos:
+
+    #Slicing video title into small title of length 50 for notification
     small_title = (video.title)[:50] if (len(video.title)>50) else video.title
+
     print(f"Downloading {small_title}")
-    # notification.notify(
-    #     title = f"Downloading {(small_title)}",
-    #     message = f"File size : {video.streams.get_highest_resolution().filesize_mb}",
-    #     app_icon = "C:/Users/HP/OneDrive/Desktop/yt-download/pythonlogoIco.ico",
-    #     timeout = 5
-    # )
+
+    #Calling progress function
     video.register_on_progress_callback(progress)
+
+    #Getting highest resolution of video
     high_def_video = video.streams.get_highest_resolution()
+
+    #Downloading the video
     high_def_video.download(output_path=download_path)
+
+    #Notifing user
     print(f"\n {small_title} downloaded successfully")
     notification.notify(
         title = small_title,
@@ -45,5 +56,7 @@ for video in p.videos:
         app_icon = "C:/Users/HP/OneDrive/Desktop/yt-download/pythonlogoIco.ico",
         timeout = 5
     )
+
+    #Pause for 3 seconds
     time.sleep(3)
 
