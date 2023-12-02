ff2905c3db6dade10adb11745f073ec552227583
Abhinandan G <abhigouli032@gmail.com>
2023-12-02 13:15:05 +0530

    ytdownload.py modified

--- ytplaylistdownload.py
+++ ytplaylistdownload.py
@@ -1,12 +1,25 @@
 from pytube import Playlist
 from sys import argv
+import time
+
+def progress(streams, chunk: bytes, bytes_remaining: int):
+    contentsize = high_def_video.filesize
+    size = contentsize - bytes_remaining
+
+    print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
+    '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
 
 link = argv[1]
 
+download_path = "C:/Users/HP/Downloads"
+
 p = Playlist(link)
 
+print(f"Title : {p.title}")
 for video in p.videos:
-    yt=video.streams.get_highest_resolution()
-    yt.download("C:/Users/HP/Downloads")
-    print(f"Downloaded {video.title}")
+    print(f"video title : {video.title}")
+    video.register_on_progress_callback(progress)
+    high_def_video = video.streams.get_highest_resolution()
+    high_def_video.download(output_path=download_path)
+    print(f"{video.title} downloaded successfully")
 
