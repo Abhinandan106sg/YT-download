de694ec3687ddded6884efbdd495d81a2bbdf09f
Abhinandan G <abhigouli032@gmail.com>
2023-12-02 19:00:25 +0530

    corrected the paths of batch files

--- playlistbatch.bat
+++ playlistbatch.bat
@@ -1,3 +1,4 @@
 @echo off
-"C:\Users\HP\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\HP\abhi.c\python scripts\playlistbatch.py"
+color a
+"C:\Users\HP\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\HP\OneDrive\Desktop\yt-download\playlistbatch.py"
 pause
--- videobatch.bat
+++ videobatch.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\HP\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\HP\abhi.c\python scripts\videobatch.py"
+"C:\Users\HP\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\HP\OneDrive\Desktop\yt-download\videobatch.py"
 pause
