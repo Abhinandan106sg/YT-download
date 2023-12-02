7790921d32b0029cb75b48abcfa7eb2aa81df66d
Abhinandan G <abhigouli032@gmail.com>
2023-12-02 18:51:37 +0530

    added batch files for video and playlist download

---
+++ playlistbatch.py
@@ -0,0 +1,69 @@
+from pytube import Playlist
+from pytube.exceptions import VideoUnavailable
+from plyer import notification
+import time
+
+#Function for progress bar in command prompt
+def progress(streams, chunk: bytes, bytes_remaining: int):
+    contentsize = high_def_video.filesize
+    size = contentsize - bytes_remaining
+
+    print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
+    '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
+
+#input playlist link
+link = input("Enter the Playlist link : ")
+
+#setting download path
+download_path = "C:/Users/HP/Downloads"
+
+#creating object of playlist class
+p = Playlist(link)
+
+#Notifing user
+print(f"Title : {p.title}")
+notification.notify(
+        title = f"Downloading {(p.title)}",
+        message = f"{len(p)} videos",
+        app_icon = "pythonlogoIco.ico",
+        timeout = 5
+    )
+
+time.sleep(3)
+
+#Iterating over each video in playlist
+for video in p.videos:
+
+    #Slicing video title into small title of length 50 for notification
+    small_title = (video.title)[:50] if (len(video.title)>50) else video.title
+
+    print(f"Downloading {video.title}")
+
+    #Calling progress function
+    video.register_on_progress_callback(progress)
+
+    try:
+
+        #Getting highest resolution of video
+        high_def_video = video.streams.get_highest_resolution()
+    
+    except VideoUnavailable:
+        print(f"{video.title} unavailable ......... skipping ")
+
+    else:
+
+        #Downloading the video
+        high_def_video.download(output_path=download_path)
+
+        #Notifing user
+        print(f"\n {video.title} downloaded successfully")
+        notification.notify(
+            title = small_title,
+            message = f"Download complete \n {download_path}",
+            app_icon = "pythonlogoIco.ico",
+            timeout = 5
+        )
+
+        #Pause for 3 seconds
+        time.sleep(3)
+
