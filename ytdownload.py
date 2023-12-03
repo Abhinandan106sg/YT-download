c2891cd15401d1b50a52eb4404fa525c5ff40913
Abhinandan G <abhigouli032@gmail.com>
2023-12-03 23:37:18 +0530

    modified ytdownload.py

--- ytdownload.py
+++ ytdownload.py
@@ -15,6 +15,7 @@ def progress(streams, chunk: bytes, bytes_remaining: int):
 
     print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
     '█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
+    # print((size/contentsize)*100)
     
 try:
     
@@ -36,7 +37,7 @@ try:
     )
 
     #Getting highest resolution of video
-    video=yt.streams.get_highest_resolution();
+    video=yt.streams.get_highest_resolution()
 
     #Downloading the video
     video.download(output_path=download_path)
